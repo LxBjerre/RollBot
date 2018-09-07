fbb894a83d67da753fe1b8f5f512fb99ace0f6c8
LxBjerre <43073674+LxBjerre@users.noreply.github.com>
2018-09-07 18:57:34 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,22 +3,19 @@ from discord.ext.commands import Bot
 from discord.ext import commands
 import asyncio
 import time
+import os
 
 Client = discord.Client()
-client = commands.Bot(command_prefix = "?")
+client = commands.Bot(command_prefix = ".")
 
 @client.event
-async def on_ready():
-print("Bot is ready")
+    async def on_ready():
+    print("Bot is ready")
+    await client.change_pressence(game=discord.Game(name="wow"))
 
 @client.event
 async def on_message(message):
     if message.content == "#roll":
-        await client.send_message(message.channel, ":cookie:")
+        await client.send_message(message.channel, "(0.author.mention) :cookie:")
 
-
-
-
-
-client.run("NDg3NjA3Mjg0NTU0NTMwODI2.DnQwhw.pgtEQYlOso9tBqW9K9UnfJn9C_Y")
-
+client.run(os.getenv("NDg3NjA3Mjg0NTU0NTMwODI2.DnQwhw.pgtEQYlOso9tBqW9K9UnfJn9C_Y"))
