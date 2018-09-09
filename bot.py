33db565d2af8c2a0868d7e94b225204ff6d6446a
LxBjerre <43073674+LxBjerre@users.noreply.github.com>
2018-09-09 12:42:18 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,19 +1,1 @@
-import discord
-from discord.ext.commands import Bot
-from discord.ext import commands
-import asyncio
-import time
 
-Client = discord.Client()
-client = commands.Bot(command_prefix = ".")
-
-@client.event
-async def on_ready():
-    print("Bot is ready")
-    
-@client.event
-async def on_message(message):
-    if message.content == "#roll":
-        await client.send_message(message.channel, "(0.author.mention) :cookie:")
-
-client.run("NDg3NjA3Mjg0NTU0NTMwODI2.DnQwhw.pgtEQYlOso9tBqW9K9UnfJn9C_Y")
